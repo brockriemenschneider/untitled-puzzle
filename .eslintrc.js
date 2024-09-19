fe5e305215a7cbed6a136bb0f6ff4babea42045a
Brock Riemenschneider <brock.riemenschneider@gmail.com>
2024-09-19 14:10:20 -0500

    converted spaces to tabs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,16 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "prettier"],
-  plugins: ["prettier"],
-  rules: {
-    "prettier/prettier": "error",
-  },
+	extends: ['expo', 'prettier'],
+	plugins: ['prettier'],
+	rules: {
+		indent: ['error', 'tab'],
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				useTabs: true,
+				tabWidth: 2,
+			},
+		],
+	},
 };
