f8d74b176f927b7867703ecc3d2133ef310292ac
Brock Riemenschneider <brock.riemenschneider@gmail.com>
2024-09-20 12:43:52 -0500

    added expo font

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
+import { Inter_400Regular, useFonts } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
+import { useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+enum FontFamily {
+	Inter_400Regular = 'Inter_400Regular',
+}
+
 export default function App() {
+	let [fontsLoaded] = useFonts({
+		[FontFamily.Inter_400Regular]: Inter_400Regular,
+	});
+
+	// Hide the splash screen once fonts are loaded and layout is ready
+	const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded) {
+			await SplashScreen.hideAsync();
+		}
+	}, [fontsLoaded]);
+
+	if (!fontsLoaded) {
+		return null;
+	}
+
 	return (
-		<View style={styles.container}>
+		<View style={styles.container} onLayout={onLayoutRootView}>
 			<Text>Open up App.tsx to start working on your app!</Text>
 			<StatusBar style="auto" />
 		</View>
@@ -16,5 +41,6 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+		fontFamily: FontFamily.Inter_400Regular,
 	},
 });
