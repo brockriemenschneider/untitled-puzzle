6e332a9829977be4ea630f1bcb87f94b7e467835
Brock Riemenschneider <brock.riemenschneider@gmail.com>
2024-09-21 14:14:35 -0500

    added additional fonts

--- src/hooks/useResources.ts
+++ src/hooks/useResources.ts
@@ -1,9 +1,18 @@
 import { FontFamily } from '@/theme/font';
-import { Inter_400Regular, useFonts } from '@expo-google-fonts/inter';
+import {
+	Inter_400Regular,
+	Inter_500Medium,
+	Inter_600SemiBold,
+	Inter_700Bold,
+	useFonts,
+} from '@expo-google-fonts/inter';
 
 export function useResources() {
 	const [fontsLoaded] = useFonts({
 		[FontFamily.Inter_400Regular]: Inter_400Regular,
+		[FontFamily.Inter_500Medium]: Inter_500Medium,
+		[FontFamily.Inter_600SemiBold]: Inter_600SemiBold,
+		[FontFamily.Inter_700Bold]: Inter_700Bold,
 	});
 
 	return fontsLoaded;
--- src/theme/font.ts
+++ src/theme/font.ts
@@ -1,3 +1,6 @@
 export enum FontFamily {
 	Inter_400Regular = 'Inter_400Regular',
+	Inter_500Medium = 'Inter_500Medium',
+	Inter_600SemiBold = 'Inter_600SemiBold',
+	Inter_700Bold = 'Inter_700Bold',
 }
